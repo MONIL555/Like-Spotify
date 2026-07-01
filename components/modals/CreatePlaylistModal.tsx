55b6cffcedae50ddeca1f6adb06136fcf13715eb
MONIL555 <monilsolanki30@gmail.com>
2026-07-01 12:56:47 +0530

    feat: add CreatePlaylistModal component for user playlist creation

---
+++ components/modals/CreatePlaylistModal.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useState } from 'react';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
+interface CreatePlaylistModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export function CreatePlaylistModal({ isOpen, onClose }: CreatePlaylistModalProps) {
+  const [name, setName] = useState('');
+
+  const handleCreate = async () => {
+    // Logic to create playlist goes here
+    onClose();
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={(open) => !open && onClose()}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create Playlist</DialogTitle>
+          <DialogDescription>
+            Give your new playlist a name.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="py-4">
+          <Input 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="My awesome playlist"
+          />
+        </div>
+        <DialogFooter>
+          <Button variant="outline" onClick={onClose}>Cancel</Button>
+          <Button onClick={handleCreate}>Create</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
